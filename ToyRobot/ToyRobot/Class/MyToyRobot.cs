3223f582f946e73eaadeaf1fd96643b89b3c3442
Ben Marino <bemarino@gmail.com>
2024-04-01 14:36:28 +1000

    Tech Test
    Tech Test

---
+++ ToyRobot/ToyRobot/Class/MyToyRobot.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobot.Class
+{
+      class MyToyRobot
+    {
+        public string Facing { get; set; }
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool Place(int x,int y,string sFacing)
+        {
+            
+            if (x>5 || y>5) return false;
+            if (x<0 || y<0) return false;
+            X = x;
+            Y = y;
+            Facing = sFacing;
+            return true;
+        }
+        public bool Move()
+        {
+            if (Facing == "NORTH")
+            {
+                if (Y == 5)
+                return false;
+                else
+                {
+                    Y = Y + 1;
+                    return true;
+                }
+            }
+            else if(Facing == "EAST") { 
+                if (X == 5)
+                    return false;
+                else
+                {
+                    X = X + 1;
+                    return true;
+                }
+            }
+            else if (Facing == "WEST")
+            {
+                if (X==0)
+                    return false;
+                else
+                {
+                    X = X - 1; return true;
+                }
+            }
+            else if (Facing == "SOUTH")
+            {
+                if (Y==0) return false;
+                else
+                {
+                    Y=Y-1; return true;
+                }
+            }
+            return false;
+        }
+        public bool Left()
+        {
+            if (Facing == "NORTH")
+            {
+                Facing = "WEST";
+                return true;
+            }
+            if (Facing == "WEST")
+            {
+                Facing = "SOUTH";
+                return true;
+            }
+            if (Facing == "SOUTH")
+            {
+                Facing = "EAST";
+                return true;
+            }
+            if (Facing == "EAST")
+            {
+                Facing = "NORTH";
+                return true;
+            }
+            else
+            return false;
+        }
+        public bool Right()
+        {
+            if (Facing == "NORTH")
+            {
+                Facing = "EAST";
+                return true;
+            }
+            if (Facing == "WEST")
+            {
+                Facing = "NORTH";
+                return true;
+            }
+            if (Facing == "SOUTH")
+            {
+                Facing = "WEST";
+                return true;
+            }
+            if (Facing == "EAST")
+            {
+                Facing = "SOUTH";
+                return true;
+            }
+            else
+                return false;
+        }
+        public string Report()
+        {
+            return " " + X.ToString() + " " + Y.ToString() + " " + Facing.ToString();
+        }
+
+    }
+}
---
+++ ToyRobot/ToyRobot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobot.Class;
+
+namespace ToyRobot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyToyRobot myToyRobot = new MyToyRobot();
+            myToyRobot.Place(0, 0, "NORTH");
+            myToyRobot.Move();
+            Console.WriteLine(myToyRobot.Report());
+            myToyRobot.Place(0, 0, "NORTH");
+            myToyRobot.Left();
+            Console.WriteLine(myToyRobot.Report());
+            myToyRobot.Place(1, 2, "EAST");
+            myToyRobot.Move();
+            myToyRobot.Move();
+            myToyRobot.Left();
+            myToyRobot.Move();
+            Console.WriteLine(myToyRobot.Report());
+            Console.ReadKey();
+        }
+    }
+}
